f17fe5afb0406a5af3005fedd08dd103d9eb38b4
VladeanClaudiu <35781140+VladeanClaudiu@users.noreply.github.com>
2022-06-13 14:53:31 +0100

    deconstructed data parsed into render function

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ const monster = {
 };
 
 function renderCharacter(data) {
-  document.getElementById(data.elementId).innerHTML = `  
+  const { elementId, name, avatar, health, roll } = data;
+  document.getElementById(elementId).innerHTML = `  
     <div class="character-card">
-    <h4 class="name">${data.name}</h4>
-    <img class="avatar" src=${data.avatar} />
-    <p class="health">health: <b> ${data.health} </b></p>
-    <div class="dice-container"><div class="dice">${data.roll}</div></div>
+    <h4 class="name">${name}</h4>
+    <img class="avatar" src=${avatar} />
+    <p class="health">health: <b> ${health} </b></p>
+    <div class="dice-container"><div class="dice">${roll}</div></div>
     </div>
         `;
 }
