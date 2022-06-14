ac2c3cb2acf04cde59632cfa9855bc4cf908d1db
VladeanClaudiu <35781140+VladeanClaudiu@users.noreply.github.com>
2022-06-14 18:59:34 +0100

    made some changes to the character roll (now generates random dice rolls dependince on the dice count)

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+//diceRoll function
+function getDiceRollArray(rollCount) {
+  let diceArray = [];
+  for (let i = 0; i < rollCount; i++) {
+    diceArray.push(Math.floor(Math.random() * 6) + 1);
+  }
+  return diceArray;
+}
+
+//get Dice Roll function
+function getDiceRollHtml(rollCount) {
+  return getDiceRollArray(rollCount)
+    .map((number) => {
+      return `<div class="dice">${number}</div>`;
+    })
+    .join("");
+}
+
 //player object
 const hero = {
   elementId: "hero",
   name: "Wizard",
   avatar: "images/wizard.png",
   health: 100,
-  roll: [3, 1, 4],
   rollCount: 3,
 };
 
@@ -14,16 +31,13 @@ const monster = {
   name: "Orc",
   avatar: "images/orc.png",
   health: 10,
-  roll: [2],
   rollCount: 1,
 };
 
 function renderCharacter(data) {
-  const { elementId, name, avatar, health, roll, rollCount } = data;
-  let rollHtml = ``;
-  for (let i = 0; i < rollCount; i++) {
-    rollHtml += `<div class="dice">${roll[i]}</div>`;
-  }
+  const { elementId, name, avatar, health, rollCount } = data;
+  const rollHtml = getDiceRollHtml(rollCount);
+
   document.getElementById(elementId).innerHTML = `  
     <div class="character-card">
     <h4 class="name">${name}</h4>
