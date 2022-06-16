99a274ac6797da517657b03f39550325b7ab29c7
VladeanClaudiu <35781140+VladeanClaudiu@users.noreply.github.com>
2022-06-16 15:19:39 +0100

    some housekeping and code formatting

--- data.js
+++ data.js
@@ -1,14 +1,12 @@
 //character data
 const characterData = {
   hero: {
-    elementId: "hero",
     name: "Wizard",
     avatar: "images/wizard.png",
     health: 100,
     rollCount: 3,
   },
   monster: {
-    elementId: "monster",
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 import characterData from "./data.js";
 import Char from "./Character.js";
 
+//render html for the hero and the monster
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+}
+
 //create new instances af the characters
 const wizard = new Char(characterData.hero);
 const orc = new Char(characterData.monster);
 
-//render html for the hero and the monster
-function render() {
-  document.getElementById(wizard.elementId).innerHTML =
-    wizard.getCharacterHtml();
-  document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
-}
-
-//calling the render function
+//rendering the characters
 render();
