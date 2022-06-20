15a75f404b3d0423e463cea1692d20fb3d2078b9
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-06-20 15:03:52 +0100

    adjusted the base stat data for some of the characters in the data js file

--- data.js
+++ data.js
@@ -3,14 +3,14 @@ const characterData = {
   hero: {
     name: "Wizard",
     avatar: "images/wizard.png",
-    health: 100,
+    health: 50,
     rollCount: 3,
     currentRollScore: [],
   },
   monster: {
     name: "Orc",
     avatar: "images/orc.png",
-    health: 10,
+    health: 20,
     rollCount: 1,
     currentRollScore: [],
   },
