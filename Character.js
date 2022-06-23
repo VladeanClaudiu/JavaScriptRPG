f48082b93b20d7caf7ee8f63c902ae2f05bf6d83
ClaudiuVladean <claudiuver3.2@gmail.com>
2022-06-23 14:20:30 +0100

    refactored the code so that character constructor is a class

--- Character.js
+++ Character.js
@@ -1,13 +1,13 @@
 import { getDiceRollArray, getDicePlaceholderHtml, getPercentace } from "./utilities.js";
 
-function Character(data) {
-  Object.assign(this, data);
+class Character{
+  constructor(data) {
+    Object.assign(this, data)
+      //store the max health of the character 
+    this.maxHealth = this.health;
 
-  //store the max health of the character 
-  this.maxHealth = this.health;
-
-  //get placeholder dice roll
-  this.rollHtml = getDicePlaceholderHtml(this.rollCount);
+    //get placeholder dice roll
+    this.rollHtml = getDicePlaceholderHtml(this.rollCount);
 
     //get Dice Roll function
     this.setDiceRollHtml = () => {
@@ -15,42 +15,39 @@ function Character(data) {
       this.rollHtml = this.currentRollScore.map( num => 
         `<div class="dice">${ num }</div>`).join('');
     };
+      
+    //take damage method
+    this.takeDamage = attackScoreArray => {
+      const totalAttackScore = attackScoreArray.reduce((total, currentEl) => {
+        return total + currentEl;
+      })
 
-    
-  //take damage method
-  this.takeDamage = attackScoreArray => {
-    const totalAttackScore = attackScoreArray.reduce((total, currentEl) => {
-      return total + currentEl;
-    })
+      //reducing character health
+      this.health -= totalAttackScore;
 
-    //reducing character health
-    this.health -= totalAttackScore;
+      //sopping character health from going below 0
+      if(this.health <= 0){
+        this.health = 0;
+        this.dead = true;
+        console.log(this.dead)
+      }
 
-    //sopping character health from going below 0
-    if(this.health <= 0){
-      this.health = 0;
-      this.dead = true;
-      console.log(this.dead)
     }
 
+    //healthbar method
+    this.getHealthBarHtml = () => {
+      const percentage = Math.floor(getPercentace(this.maxHealth, this.health));
     
-  }
+      return `<div class="health-bar-outer">
+                  <div class="health-bar-inner ${ percentage < 26 ? 'danger' : '' } " 
+                      style="width: ${percentage}%;">
+                  </div>
+              </div>`
 
-  //healthbar method
-  this.getHealthBarHtml = () => {
-    const percentage = Math.floor(getPercentace(this.maxHealth, this.health));
-   
-    return `<div class="health-bar-outer">
-                <div class="health-bar-inner ${ percentage < 26 ? 'danger' : '' } " 
-                    style="width: ${percentage}%;">
-                </div>
-            </div>`
+    }
 
   }
-
-
-  //character html
-  this.getCharacterHtml = () => {
+  getCharacterHtml(){
     const { name, avatar, health, rollHtml } = this;
     const healthBar = this.getHealthBarHtml();
     return `  
@@ -63,8 +60,8 @@ function Character(data) {
           ${rollHtml}
         </div>
       </div>
-          `;
-  };
+          `
+  }
 }
 
 
